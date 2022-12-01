45c5048ff607fd7dafaef72d153db72d64d1a3d7
Ralph Niemitz <ralph.niemitz@gmx.de>
2022-12-01 21:25:24 +0100

    Cleaned up some dependencies

--- src/test/java/de/ralleytn/simple/json/TestJSONArray.java
+++ src/test/java/de/ralleytn/simple/json/TestJSONArray.java
@@ -203,8 +203,6 @@
  */
 package de.ralleytn.simple.json;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -224,6 +222,8 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
